4e7111465b811b1fcb5a3c97e083b6b2f279a44e
Jeff Schumacher <thegreatjerboa@gmail.com>
2015-08-06 20:00:12 -0500

    Update for tank and/or arcade drive

--- src/Cannon/src/com/frc2508/cannon/Main.java
+++ src/Cannon/src/com/frc2508/cannon/Main.java
@@ -4,11 +4,10 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package com.frc2508.cannon;
 
-
 import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
@@ -23,25 +22,25 @@ import edu.wpi.first.wpilibj.can.CANTimeoutException;
  * directory.
  */
 public class Main extends SimpleRobot {
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
+
     }
 
     CANJaguar[] motors = new CANJaguar[4];
-    
+
     RobotDrive robotDrive;
 
     Joystick moveStick, rotateStick;
     /*
      * This function is called once each time the robot enters operator control.
-     */    
-    private void configJaguar(int index, int id)
-    {
-        try
-        {
+     */
+
+    private void configJaguar(int index, int id) {
+        try {
             CANJaguar jag = new CANJaguar(id, CANJaguar.ControlMode.kSpeed);
             jag.setSpeedReference(CANJaguar.SpeedReference.kQuadEncoder);
             jag.setSafetyEnabled(false);
@@ -49,62 +48,105 @@ public class Main extends SimpleRobot {
             jag.configEncoderCodesPerRev(360);
             jag.setPID(.12, .02, 0);
             jag.enableControl();
-            
-             motors[index] = jag;
-        } 
-        catch (CANTimeoutException ex) 
-        {
+
+            motors[index] = jag;
+        } catch (CANTimeoutException ex) {
             ex.printStackTrace();
         }
     }
 
-    public void configureJaguars()
-    {
+    public void configureJaguars() {
         configJaguar(0, 8);
         configJaguar(1, 2);
         configJaguar(2, 3);
         configJaguar(3, 4);
-        
+
     }
-    public void operatorControl() 
-    {
+
+    public void operatorControl() {
         configureJaguars();
-        
+
         moveStick = new Joystick(1);
         rotateStick = new Joystick(2);
-        
+
         robotDrive = new RobotDrive(motors[1], motors[0], motors[2], motors[3]);
-        
+
         robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
 
         double gearRatio = 12.0;
         double maxRPM = 5310.0;
-        robotDrive.setMaxOutput((maxRPM / gearRatio) / 5);        
-        
-        while (isOperatorControl() && isEnabled()) {
-            double inX = moveStick.getX();
-            double inY = moveStick.getY();
-            double inTwist = moveStick.getTwist();
-            //System.out.println("x: " + inX);
-            //System.out.println("y: " + inY);
-            //System.out.println("twist: " + inTwist);
+        robotDrive.setMaxOutput((maxRPM / gearRatio) / 5);
 
-            double deadBand = .05;
-            double outX = inX > deadBand || inX < -1 * deadBand ? inX : 0;
-            double outY = inY > deadBand || inY < -1 * deadBand ? inY : 0;
-            
-            double invertedOutY = -1 * outY;
-            double invertedTwist = -1 * inTwist;
-            
-            robotDrive.mecanumDrive_Cartesian(outX, invertedOutY, invertedTwist,0);
+        while (isOperatorControl() && isEnabled()) {
+            //doMecanum();
+            //doTank();            
+            doArcade();
+            printMoveStickAxis();
+
             Timer.delay(.01);
-        }        
+        }
+
     }
-    
+
+    public double ramp(double input) {
+        if (input == 0) {
+            return 0;
+        }
+
+        return input * Math.abs(input);
+    }
+
+    public double deadband(double input) {
+        double deadBand = .05;
+        return input > deadBand || input < -1 * deadBand ? input : 0;
+    }
+
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
-    
+
+    }
+
+    private void doMecanum() {
+        double inX = moveStick.getX();
+        double inY = moveStick.getY();
+        double inTwist = moveStick.getTwist();
+
+        double outX = deadband(inX);
+        double outY = deadband(inY);
+
+        double invertedOutY = -1 * outY;
+        double invertedTwist = -1 * inTwist;
+
+        outX = ramp(outX);
+        // outY = ramp(outY);
+        invertedOutY = ramp(invertedOutY);
+        invertedTwist = ramp(invertedTwist);
+
+        robotDrive.mecanumDrive_Cartesian(outX, invertedOutY, invertedTwist, 0);
+    }
+
+    private void doTank() {
+        double inLeft = moveStick.getRawAxis(2);
+        double inRight = -moveStick.getRawAxis(4);
+
+        double outLeft = ramp(deadband(inLeft));
+        double outRight = ramp(deadband(inRight));
+
+        robotDrive.tankDrive(outLeft, outRight);
+    }
+    private void doArcade() {
+        double inMove = moveStick.getRawAxis(1);
+        double inRotate = moveStick.getRawAxis(2);
+        
+        
+        robotDrive.arcadeDrive(inMove,inRotate,true);
+    }
+
+    private void printMoveStickAxis() {
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Axis:" + i + " Value:" + moveStick.getRawAxis(i));
+        }
     }
 }
